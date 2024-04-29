3059cc6b4718c19498ceb44254705b1a66f843df
abdullah <abdu44ll3ah55@gmail.com>
2024-04-29 18:09:51 +0300

    tailwind

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="bg-gray-800 text-white p-4">
+            <ul className="flex space-x-4">
+                <li>
+                    <Link to="/" className="hover:bg-gray-700 px-3 py-2 rounded">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about" className="hover:bg-gray-700 px-3 py-2 rounded">About</Link>
+                </li>
+                <li>
+                    <Link to="/services" className="hover:bg-gray-700 px-3 py-2 rounded">Services</Link>
+                </li>
+                <li>
+                    <Link to="/contact" className="hover:bg-gray-700 px-3 py-2 rounded">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
